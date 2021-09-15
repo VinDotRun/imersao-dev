5ae072f17d32ca32b1c5d38db613638f6d7eafc2
VinDotRun <28811713+VinDotRun@users.noreply.github.com>
2021-09-15 15:12:58 -0300

    Aula 03

---
+++ aula-03/script.js
@@ -0,0 +1,16 @@
+function Chutar() {
+  var numeroSecreto = parseInt(Math.random() * 11);
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+
+  console.log(chute);
+
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou!";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve digitar um número entre 0 e 10";
+  } else {
+    elementoResultado.innerHTML =
+      "Você errou! O número secreto era " + numeroSecreto;
+  }
+}
